f9cbe9f2f58771daa548896867498b368f647691
Jonathan Gin <jonathangin52@gmail.com>
2019-05-01 15:22:24 -0400

    MVP game

--- tic_tac_toe/src/main.rs
+++ tic_tac_toe/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::char;
 
 struct Player {
     board: u16,
@@ -24,12 +25,78 @@ impl Player {
         }
         false
     }
+
+    fn place_token(&mut self, position: u32) -> bool {
+        self.board += 2u16.pow(position - 1);
+        self.check_win()
+    }
 }
 
 fn main() {
-    let player1 = Player {
+    println!("Welcome to tic tac toe");
+    let mut turn = 0;
+    let mut player1 = Player {
         board: 0,
         token: 'X',
     };
-    println!("Welcome to tic tac toe");
+    let mut player2 = Player {
+        board: 0,
+        token: 'O',
+    };
+    while turn <= 9 {
+        draw_board(&player1, &player2);
+        let position = get_move(player1.board, player2.board);
+        let game_over;
+        if turn % 2 == 0 {
+            game_over = player1.place_token(position);
+        } else {
+            game_over = player2.place_token(position);
+        }
+        if game_over {
+            break;
+        }
+        turn += 1;
+    }
+}
+
+fn draw_board(player1: &Player, player2: &Player) {
+    let mut output = String::with_capacity(31);
+    for n in 0..=8 {
+        if n != 0 && n % 3 == 0 {
+            output += "\n- - - \n";
+        }
+        let token = if check_bit(player1.board, n) {
+            player1.token
+        } else if check_bit(player2.board, n) {
+            player2.token
+        } else {
+            char::from_digit((n + 1) as u32, 10).unwrap_or(' ')
+        };
+        output.push(token);
+        if (n + 1) % 3 != 0 {
+            output.push('|');
+        }
+    }
+    println!("{}", output);
+}
+
+fn get_move(board1: u16, board2: u16) -> u32 {
+    loop {
+        println!("Please input a position from 1-9.");
+
+        let mut input = String::new();
+
+        io::stdin().read_line(&mut input)
+            .expect("Failed to read line");
+
+        let input: u32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+        return input;
+    }
+}
+
+fn check_bit(input: u16, n: u8) -> bool {
+    input & (1 << n) != 0
 }
